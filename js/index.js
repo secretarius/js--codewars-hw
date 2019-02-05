7d9bb8a027f220f64bb4f4872f4f48ee5b3c0ccf
Alex <secretarius@ukr.net>
2019-02-05 22:49:03 +0000

    formatedcode

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 //Grasshopper - Terminal game combat function
 function combat(health, damage) {
-  let res = health - damage;
-    if(res < 0){
-      return 0;
-    }else{
-      return res
-      }
+    let res = health - damage;
+    if (res < 0) {
+        return 0;
+    } else {
+        return res
+    }
 }
 alert(combat(100, 5));
 alert(combat(20, 30));
@@ -13,28 +13,26 @@ alert(combat(20, 30));
 //Find the smallest integer in the array
 class SmallestIntegerFinder {
     findSmallestInt(args) {
- return args.reduce((a, b) => Math.min(a, b));
-     
-     } 
-  
+        return args.reduce((a,b)=>Math.min(a, b));
+
+    }
+
 }
 var sif = new SmallestIntegerFinder();
-alert(sif.findSmallestInt([78,56,232,12,8]));
-alert(sif.findSmallestInt([78,56,232,12,18]));
+alert(sif.findSmallestInt([78, 56, 232, 12, 8]));
+alert(sif.findSmallestInt([78, 56, 232, 12, 18]));
 
 //Lost Without a Map
-function maps(x){
-  return x.map((a) => (a * 2));
+function maps(x) {
+    return x.map((a)=>(a * 2));
 }
 
 alert(maps([1, 2, 3]))
 
 //Kata: How new Works
-myObj = {};
-myObj.__proto__ = MyObject.prototype;
+myObj = {}
+myObj.__proto__ = MyObject.prototype
 myObj.constructor(MyObject)
-
-
 
 // const factory = function () {
 //   this.__proto__ = MyObject.prototype;
@@ -42,4 +40,5 @@ myObj.constructor(MyObject)
 // };
 //  var myObj = new factory();
 //factory.call(myObj);
+
 
